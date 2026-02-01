9900ea01cab613ebfeeaa6d51e329bdf978422c5
Saif Tandel <saifcodes13@gmail.com>
2026-02-01 05:59:20 +0530

    fix: correct cookie config for deployed auth

--- server/utils/generate-token-utils.js
+++ server/utils/generate-token-utils.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        secure: true , //on development mode i was running on "process.env.NODE_ENV !== "development""
+        sameSite: "none", //on development mode i was running on "strict" mode
         maxAge: 7 * 24 * 60 * 60 * 1000
 
     })
